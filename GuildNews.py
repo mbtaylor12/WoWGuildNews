01a44434bd327d069c5467ce85967930bb3f9377
mbtaylor12 <mbtaylor12@hotmail.com>
2017-11-05 16:52:15 -0500

    Getting latest news item works.

--- GuildNews.py
+++ GuildNews.py
@@ -1,4 +1,30 @@
 import json
 import urllib
 
+API_INFO=open("API_INFO.txt", "r")
+
+REALMNAME=API_INFO.readline()[11:].replace(" ", "%20")
+GUILDNAME=API_INFO.readline()[11:].replace(" ", "%20")
+LOCALE=API_INFO.readline()[7:]
+APIKEY=API_INFO.readline()[7:]
+
+API_INFO.close()
+
 def getLatestNews():
+	url="https://us.api.battle.net/wow/guild/" + REALMNAME + "/" + GUILDNAME + "?fields=news&locale=" + LOCALE + "&apikey=" + APIKEY
+	response=urllib.urlopen(url)
+	jsonData=json.loads(response.read())
+	newsData=jsonData["news"][0]
+
+	character=newsData["character"]
+	message=character
+
+	if(newsData["type"]=="playerAchievement"):
+		achievementTitle=newsData["achievement"]["title"]
+		message = message + " earned " + achievementTitle
+	elif("item" in newsData["type"]):
+		item=newsData["itemId"]
+		message = message + " has received the item " + str(item)
+
+
+	return message
