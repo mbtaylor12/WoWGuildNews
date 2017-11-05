022640426d98b6f3dcc7f8f705af879ea6493d43
mbtaylor12 <mbtaylor12@hotmail.com>
2017-11-05 17:08:24 -0500

    The item ID now gets resolved to a quality and name.

--- GuildNews.py
+++ GuildNews.py
@@ -10,6 +10,32 @@ APIKEY=API_INFO.readline()[7:]
 
 API_INFO.close()
 
+def resolveItemQuality(itemID):
+	return{
+		0 : "poor",
+		1 : "common",
+		2 : "uncommon",
+		3 : "rare",
+		4 : "epic",
+		5 : "legendary",
+		6 : "artifact",
+		7 : "heirloom",
+		8 : "WoW Token"
+	}[itemID]
+
+def resolveItemInfo(itemID):
+	url="https://us.api.battle.net/wow/item/" + str(itemID) + "?locale=" + LOCALE + "&apikey=" + APIKEY
+	response=urllib.urlopen(url)
+	jsonData=json.loads(response.read())
+
+	itemInfo={}
+
+	itemInfo["name"]=jsonData["name"]
+	itemInfo["quality"]=resolveItemQuality(jsonData["quality"])
+
+	return itemInfo
+
+
 def getLatestNews():
 	url="https://us.api.battle.net/wow/guild/" + REALMNAME + "/" + GUILDNAME + "?fields=news&locale=" + LOCALE + "&apikey=" + APIKEY
 	response=urllib.urlopen(url)
@@ -24,7 +50,10 @@ def getLatestNews():
 		message = message + " earned " + achievementTitle
 	elif("item" in newsData["type"]):
 		item=newsData["itemId"]
-		message = message + " has received the item " + str(item)
+		itemInfo=resolveItemInfo(item)
+		message = message + " has received the " + str(itemInfo["quality"]) + " item " + itemInfo["name"]
 
 
 	return message
+
+print getLatestNews()
